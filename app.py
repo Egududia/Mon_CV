afb2dd9224782cbd30108cd2f9e21831b30e78d7
Egududia <adamaegudu@gmail.com>
2026-02-25 21:24:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,5 @@ import streamlit as st
 with st.sidebar:
   st.info("Prénom: Adama Egudu")
 st.header("Profil")
+st.write("Étudiante en 2ᵉ année de BTS en géomatique,passionnée par l’analyse spatiale,le traitement d'image par drone
+et la cartographie thématique.")
