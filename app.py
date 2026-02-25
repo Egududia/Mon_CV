03909b66cb5092b52f6e7e41726a17f9558ef9b6
Egududia <adamaegudu@gmail.com>
2026-02-25 22:07:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 with st.sidebar:
-  st.info("Adama Egudu DIA" ),
-  st.info("Adresse:Camberene1" ),
+  st.info("Adama Egudu DIA" )
+  st.info("Adresse:Camberene1" )
 st.header("Profil")
 st.write("Étudiante en 2ᵉ année de BTS en géomatique,passionnée par l’analyse spatiale,le traitement d'image par drone et la cartographie thématique")
