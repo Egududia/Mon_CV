68c19b75c5cb7faea1ae555a59454be823b87abe
Egududia <adamaegudu@gmail.com>
2026-02-25 21:26:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ with st.sidebar:
   st.info("Prénom: Adama Egudu")
 st.header("Profil")
 st.write("Étudiante en 2ᵉ année de BTS en géomatique,passionnée par l’analyse spatiale,le traitement d'image par drone
-et la cartographie thématique.")
+et la cartographie thématique")
