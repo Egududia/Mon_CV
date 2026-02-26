19bf68092424853e3fc8780ae3cfbe551319a681
Egududia <adamaegudu@gmail.com>
2026-02-26 12:45:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ st.header("Profil")
 st.write("Étudiante en 2ᵉ année de BTS en géomatique,passionnée par l’analyse spatiale,le traitement d'image par drone et la cartographie thématique")
 st.header("Compétence")
 st.markdown("""
-. Maitrise des logiciel Qgis,Arcgis,Agisoft metashape, Autocad,...
-. Conception de carte
-. Géoréférencement d’images
-. Numérisation
-. Conception de bases de données spatiales
-. Pilotage de drone
-. Intégration des GCP 
+- Maitrise des logiciel Qgis,Arcgis,Agisoft metashape, Autocad,...
+- Conception de carte
+- Géoréférencement d’images
+- Numérisation
+- Conception de bases de données spatiales
+- Pilotage de drone
+- Intégration des GCP 
 """)
